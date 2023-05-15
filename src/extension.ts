c3cc27bd660eedba5114593fe64afbab9df61fb3
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2023-05-15 20:32:58 +0900

    feat: 기본적인 기능을 완성한다

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,168 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import fs from "fs";
+import * as fs from "fs";
 import { execSync } from "node:child_process";
+import path from "path";
 
-// https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-const colours = {
-  reset: "\x1b[0m",
-  bright: "\x1b[1m",
-  dim: "\x1b[2m",
-  underscore: "\x1b[4m",
-  blink: "\x1b[5m",
-  reverse: "\x1b[7m",
-  hidden: "\x1b[8m",
+function updateVersion(targetPackage: string, version: string) {
+  const file = fs.readFileSync(targetPackage, "utf8");
+  const json = JSON.parse(file);
+  json.version = version;
+  fs.writeFileSync(targetPackage, JSON.stringify(json, null, 2));
+}
 
-  fg: {
-    black: "\x1b[30m",
-    red: "\x1b[31m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
-    blue: "\x1b[34m",
-    magenta: "\x1b[35m",
-    cyan: "\x1b[36m",
-    white: "\x1b[37m",
-    gray: "\x1b[90m",
-    crimson: "\x1b[38m", // Scarlet
-  },
-  bg: {
-    black: "\x1b[40m",
-    red: "\x1b[41m",
-    green: "\x1b[42m",
-    yellow: "\x1b[43m",
-    blue: "\x1b[44m",
-    magenta: "\x1b[45m",
-    cyan: "\x1b[46m",
-    white: "\x1b[47m",
-    gray: "\x1b[100m",
-    crimson: "\x1b[48m",
-  },
-};
+function startHotfixWithProgress() {
+  vscode.window.withProgress(
+    {
+      location: vscode.ProgressLocation.Notification,
+      title: "Hotfix 시작",
+      cancellable: true,
+    },
+    (progress, token) => {
+      token.onCancellationRequested(() => {
+        console.log("User canceled the long running operation");
+      });
+
+      return startHotfix(progress);
+    }
+  );
+}
+
+async function startHotfix(
+  progress: vscode.Progress<{ message?: string; increment?: number }>
+) {
+  try {
+    const workspacePath = await initHotfix();
+    progress.report({
+      increment: 20,
+      message: "develop 브랜치를 pull 중입니다.",
+    });
+    await pullOriginDevelop(workspacePath);
+    progress.report({
+      increment: 20,
+      message: "master 브랜치를 pull 중입니다.",
+    });
+    await pullOriginMaster(workspacePath);
+    progress.report({
+      increment: 20,
+      message: "패키지 버전을 업데이트 중입니다.",
+    });
+    const hotfixVersion = await updatePackagesVersion(workspacePath);
+    progress.report({
+      increment: 20,
+      message: "hotfix 브랜치를 생성 중입니다.",
+    });
+    await checkoutToHotfixBranch(hotfixVersion);
+  } catch (error) {
+    vscode.window.showErrorMessage(error as string);
+  }
+}
+
+async function pullOriginDevelop(workspaceFolderPath: string) {
+  try {
+    execSync("git checkout develop && git pull origin develop", {
+      cwd: workspaceFolderPath,
+    });
+  } catch (error) {
+    throw new Error("develop 브랜치를 pull 할 수 없습니다.");
+  }
+}
+
+async function pullOriginMaster(workspaceFolderPath: string) {
+  try {
+    execSync("git checkout master && git pull origin master", {
+      cwd: workspaceFolderPath,
+    });
+  } catch (error) {
+    throw new Error("master 브랜치를 pull 할 수 없습니다.");
+  }
+}
+
+async function updatePackagesVersion(workspaceFolderPath: string) {
+  try {
+    const nextVersion = (() => {
+      const file = fs.readFileSync(
+        path.join(workspaceFolderPath!, "package.json"),
+        "utf-8"
+      );
+      const packageJson = JSON.parse(file);
+      const version = packageJson.version.split(".");
+      version[2] = parseInt(version[2]) + 1;
+      return version.join(".");
+    })() as string;
+
+    updateVersion(path.join(workspaceFolderPath!, "package.json"), nextVersion);
+    updateVersion(
+      path.join(workspaceFolderPath!, "package-lock.json"),
+      nextVersion
+    );
+
+    return nextVersion;
+  } catch (error) {
+    throw new Error("패키지 버전을 업데이트 할 수 없습니다.");
+  }
+}
+
+async function checkoutToHotfixBranch(hotfixVersion: string) {
+  try {
+    let terminal = vscode.window.activeTerminal;
+
+    if (!terminal) {
+      terminal = vscode.window.createTerminal();
+      terminal.show();
+    }
+
+    terminal.sendText(`git checkout -b hotfix/${hotfixVersion}`, true);
+    // execSync(`git checkout -b hotfix/${hotfixVersion}`, {
+    //   cwd: workspaceFolderPath,
+    // });
+  } catch (error) {
+    throw new Error("hotfix 브랜치를 생성할 수 없습니다.");
+  }
+}
+
+async function initHotfix() {
+  try {
+    const workspaceFolder = vscode.workspace
+      .workspaceFolders as vscode.WorkspaceFolder[];
+    const workspaceFolderPath = workspaceFolder[0]?.uri?.fsPath;
+    const output = execSync("git diff-index HEAD --", {
+      cwd: workspaceFolderPath,
+    }).toString();
+    const lines = output.split("\n").filter((line) => line.length > 0);
+    const fileNames = lines
+      .map((line) => line.split(" "))
+      .map(([, , , , fileName]) => fileName)
+      .map((fileName) => fileName.replace("M\t", ""));
+    if (fileNames.length > 0) {
+      if (fileNames.every((fileName) => fileName.includes("submodules"))) {
+        const selection = await vscode.window.showInformationMessage(
+          `submodules 변경점이 있습니다. submodule을 update 하시겠습니까?`,
+          "Yes",
+          "No"
+        );
+
+        if (selection === "Yes") {
+          execSync("git submodule update --remote", {
+            cwd: workspaceFolderPath,
+          });
+          return workspaceFolderPath;
+        }
+      }
+      vscode.window.showErrorMessage(
+        `commit 하지 않은 변경점이 있습니다. - ${fileNames
+          .map((n) => `'${n}'`)
+          .join(",")}. commit이나 stash 후 다시 시도해주세요.`
+      );
+      throw new Error("commit 하지 않은 변경점이 있습니다.");
+    }
+
+    return workspaceFolderPath;
+  } catch (error) {
+    throw new Error("Hotfix를 시작할 수 없습니다.");
+  }
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -62,16 +186,11 @@ export function activate(context: vscode.ExtensionContext) {
         return;
       }
 
-      const workspaceFolderPath = workspaceFolder[0]?.uri?.fsPath;
-
       try {
-        const output = execSync("git diff-index HEAD --").toString();
+        startHotfixWithProgress();
       } catch (error) {}
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from medistream-extension!"
-      );
     }
   );
 
